f663abca027672f71c51f299301c8ffa1023691d
programmer-ejie <programmerejie@gmail.com>
2025-07-31 12:08:38 +0800

    base codes for the website including react and springbot default files

---
+++ backend/src/main/java/com/development/Barangay_e_Services_Portal/BarangayEServicesPortalApplication.java
@@ -0,0 +1,13 @@
+package com.development.Barangay_e_Services_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarangayEServicesPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarangayEServicesPortalApplication.class, args);
+	}
+
+}

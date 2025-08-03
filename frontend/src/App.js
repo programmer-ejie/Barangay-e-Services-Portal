2dfeb696a17d06491f157d4f1daf339d2f11c781
programmer-ejie <programmerejie@gmail.com>
2025-08-03 14:28:13 +0800

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        lorem
-     asdasdasdasdsaasdasdasdasdasdassdsdsdsdsdsdsdsdsdsdsdsd
+        loremasdasasasdasdadsadsasddasdasdas
         <a
           className="App-link"
           href="https://reactjs.org"

98b39e3f3ded63bc244d433907c12a64decab427
programmer-ejie <programmerejie@gmail.com>
2025-08-03 14:31:22 +0800

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="asdasdasdaxcaxzcasdasd"
-        >
-          asdasdasdaxcaxzcasdasdasdasdasdasdasdasdasdas
-        </a>
-
-      </header>
+      
     </div>
   );
 }

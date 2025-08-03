b8c28668526bfb129f294e3de04f39c2890f2cd1
programmer-ejie <programmerejie@gmail.com>
2025-08-03 14:25:47 +0800

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         lorem
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Lorem ipsum dolor sit amet, coasdasdasdasdasdasdasdasdasdadasdansectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
         <a
           className="App-link"
